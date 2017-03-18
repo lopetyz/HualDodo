37503817dce53aadf653a3b44bede6c3d24a1b42
lopetyz <tardu@qq.com>
2017-03-19 04:10:06 +0800

    add rx and retrofit support

--- app/src/main/java/com/lopetyz/hualdodo/MainActivity.java
+++ app/src/main/java/com/lopetyz/hualdodo/MainActivity.java
@@ -26,13 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
         BtnListManager manager = new BtnListManager(this);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
